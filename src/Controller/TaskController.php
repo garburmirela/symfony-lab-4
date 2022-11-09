dec6afd3a9310a26f04fdf1b64682485f67afd6f
Garbur Mirela <garburmirela@gmail.com>
2022-11-09 22:36:58 +0200

    Create form using symfony components

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,23 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Task;
+use App\Form\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
-    public function new(Request $request): void
+    #[Route('/', name: 'home')]
+    public function new(Request $request): Response
     {
         // creates a task object and initializes some data for this example
         $task = new Task();
         $task->setTask('Write a blog post');
         $task->setDueDate(new \DateTime('tomorrow'));
 
-        $form = $this->createFormBuilder($task)
-            ->add('task', TextType::class)
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class, ['label' => 'Create Task'])
-            ->getForm();
+        $form = $this->createForm(TaskType::class, $task);
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
